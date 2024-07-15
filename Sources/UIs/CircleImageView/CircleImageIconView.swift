f82538186e70db8c61bc8756a2eae7c7e01c0f77
Thinh Nguyen <thinhnguyen12389@gmail.com>
2024-07-15 23:03:54 +0700

    feat: [TCH-12] Implement circle image icon

---
+++ Sources/UIs/CircleImageView/CircleImageIconView.swift
@@ -0,0 +1,51 @@
+//  Created by Thinh Nguyen
+//
+
+import SwiftUI
+
+public struct CircleImageIconView: View {
+  let fillColor: Color
+  let diameter: CGFloat
+  let iconColor: Color
+  let icon: UIImage
+  let iconSize: CGFloat
+
+  public init(
+    fillColor: Color,
+    diameter: CGFloat,
+    iconColor: Color,
+    icon: UIImage,
+    iconSize: CGFloat
+  ) {
+    self.fillColor = fillColor
+    self.diameter = diameter
+    self.iconColor = iconColor
+    self.icon = icon
+    self.iconSize = iconSize
+  }
+
+  public var body: some View {
+    ZStack {
+      Circle()
+        .frame(width: diameter, height: diameter)
+        .foregroundColor(fillColor)
+      Image(uiImage: icon)
+        .resizable()
+        .renderingMode(.template)
+        .frame(width: iconSize, height: iconSize, alignment: .center)
+        .foregroundColor(iconColor)
+    }
+  }
+}
+
+struct CircleImageIconView_Preview: PreviewProvider {
+  static var previews: some View {
+    CircleImageIconView(
+      fillColor: .brown,
+      diameter: 60,
+      iconColor: .red,
+      icon: UIImage(systemName: "heart.fill")!,
+      iconSize: 30
+    )
+  }
+}
