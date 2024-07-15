41bd63ec49579bc645fd1fae0511b458ddb2bdeb
Thinh Nguyen <thinhnguyen12389@gmail.com>
2024-07-15 22:32:03 +0700

    feat: [TCH-10] Implement Carousel View

---
+++ Sources/UIs/CarouselView/Carousel.swift
@@ -0,0 +1,59 @@
+//  Created by Thinh Nguyen
+//
+
+import SwiftUI
+
+struct Carousel<Content: View>: View {
+  @EnvironmentObject var themeProvider: ThemeProvider
+  @ViewBuilder let content: Content
+  @State private var index = 0
+
+  let maxIndent: Int
+
+  public init(
+    maxIndent: Int,
+    @ViewBuilder content: () -> Content
+  ) {
+    self.content = content()
+    self.maxIndent = maxIndent
+  }
+
+  var body: some View {
+    VStack {
+      TabView(selection: $index,
+              content:  {
+        ForEach(0..<maxIndent, id: \.self) { _ in
+          content
+        }
+      })
+      .tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
+      .onChange(of: index) { newValue in
+        if newValue == maxIndent {
+          index = 0
+        }
+      }
+    }
+    .frame(height: 200)
+  }
+}
+
+struct Carousel_Preview: PreviewProvider {
+  static var previews: some View {
+    Carousel(maxIndent: 3) {
+      HStack {
+        CardView(fillColor: .blue) {
+          VStack {
+            Text("Hello Title")
+              .frame(
+                maxWidth: .infinity,
+                alignment: .leading
+              )
+            Spacer()
+            Text("Hello Subtitle")
+              .frame(maxWidth: .infinity, alignment: .leading)
+          }
+        }
+      }
+    }.previewable()
+  }
+}
---
+++ Sources/UIs/CarouselView/TransitionType.swift
@@ -0,0 +1,10 @@
+//  Created by Thinh Nguyen
+//
+
+import Foundation
+
+public enum TransitionType {
+  case rotation3D,
+       scale,
+       opacity
+}
