613aa1d41061ebcc1ca4eb0e9ab63e188b271080
Nguyen Duc Thinh <thinhnguyen12389@gmail.com>
2024-08-24 22:23:19 +0700

    feat: [TCH-31] Implemented BottomCornersCell (#34)

---
+++ Sources/Enums/DesignSystem.swift
@@ -0,0 +1,14 @@
+//  Created by Thinh Nguyen
+//
+
+import Foundation
+import UIKit
+
+public enum DesignSystem {
+    static let cornerRadiusTiny: CGFloat = 2
+    static let cornerRadiusSmall: CGFloat = 4
+    static let cornerRadiusMedium: CGFloat = 8
+    static let cornerRadiusSemiLarge: CGFloat = 12
+    static let cornerRadiusLarge: CGFloat = 16
+    static let cornerRadiusLarger: CGFloat = 24
+}
