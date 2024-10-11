8dbc74055046cb6b1b0063e4cda9aa5feb3847ce
Thinh Nguyen <thinhnguyen12389@gmail.com>
2024-10-11 13:44:50 +0700

    feat: [TCH-44] Added NoResultsCell

---
+++ Sources/UIs/UIKit/UITableViewCell/NoResultsCell/NoResultsCell.swift
@@ -0,0 +1,39 @@
+//  Created by Thinh Nguyen
+//
+
+import UIKit
+
+public final class NoResultsCell: UITableViewCell {
+    public static let id = "NoResultsCell"
+
+    public let label = UILabel()
+
+    override public init(
+        style: UITableViewCell.CellStyle,
+        reuseIdentifier: String?
+    ) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+
+        selectionStyle = .none
+        backgroundColor = .clear
+
+        label.text = "None found".localised
+        label.textColor = .textBodySubtle
+        label.font = .body1
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(label)
+
+        NSLayoutConstraint.activate([
+            label.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            label.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+            label.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 12),
+            label.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -32)
+        ])
+    }
+
+    @available(*, unavailable)
+    public required init?(coder _: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
