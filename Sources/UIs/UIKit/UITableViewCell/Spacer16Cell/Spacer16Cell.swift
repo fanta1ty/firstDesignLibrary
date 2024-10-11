b561416afa98756be7070946482b1787600c118b
Thinh Nguyen <thinhnguyen12389@gmail.com>
2024-10-11 16:37:12 +0700

    Create Spacer16Cell.swift

---
+++ Sources/UIs/UIKit/UITableViewCell/Spacer16Cell/Spacer16Cell.swift
@@ -0,0 +1,34 @@
+//  Created by Thinh Nguyen
+//
+
+import UIKit
+
+public final class Spacer16Cell: UITableViewCell {
+    public static let id = "Spacer16Cell"
+
+    public let curve = UIView()
+    public var height: NSLayoutConstraint?
+
+    override public init(
+        style: UITableViewCell.CellStyle,
+        reuseIdentifier: String?
+    ) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+
+        selectionStyle = .none
+        backgroundColor = .uiBackgroundSecondary
+
+        height = contentView.heightAnchor.constraint(equalToConstant: 16)
+        height?.isActive = true
+    }
+
+    @available(*, unavailable)
+    public required init?(coder _: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    public override func prepareForReuse() {
+        super.prepareForReuse()
+        height?.constant = 16
+    }
+}
