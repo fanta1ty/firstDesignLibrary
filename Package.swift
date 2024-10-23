d0b79937ae21d0a287e4fe72ebe7b1c38c0ce309
Thịnh Nguyễn <thinhnguyen12389@gmail.com>
2024-10-23 16:25:37 +0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,11 @@
-// swift-tools-version: 5.9
+#if compiler(>=5.8)
+// Code for Swift 5.8 or newer
+// swift-tools-version: 6.0
+#else
+// Code for Swift 5.7 or older
+// swift-tools-version: 5.7
+#endif
+
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
